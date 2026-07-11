ac5547ebca88ea632e10bd4a6846c6dce60a48f5
LironeFitoussi <lironefit@icloud.com>
2026-07-11 21:35:09 +0300

    Add installation scripts for containerd, nerdctl, CNI plugins, and buildkit

---
+++ 1-docker-vs-containerd/ctr.sh
@@ -0,0 +1,9 @@
+# Installation (Linux preferred)
+# 1. Download the latest release from
+wget https://github.com/containerd/containerd/releases/download/v2.3.0/containerd-2.3.0-linux-amd64.tar.gz
+
+# 2. Extract the tarball
+tar xvf containerd-2.3.0-linux-amd64.tar.gz
+
+# 3. Test the installation
+ctr --version
